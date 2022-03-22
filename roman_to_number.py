759861c9d27ed0581331855e9d60a11241f353b5
ALİ RIZA ÇELİK <riza-celik@outlook.com>
2022-03-22 16:04:11 +0300

    it has been simplified more

--- roman_to_number.py
+++ roman_to_number.py
@@ -1,5 +1,6 @@
-def romanToInt(s):
-    romans = {
+from functools import reduce
+
+romans = {
         "I": 1,
         "V": 5,
         "X": 10,
@@ -8,14 +9,16 @@ def romanToInt(s):
         "D": 500,
         "M": 1000
     }
-    result = 0
-    current_value = ""
-    for x in s:
-        if current_value!="" and romans.get(current_value) < romans.get(x):
-            result =result+ (romans.get(x)-2*romans.get(current_value))
+def different_solving(s):
+    return reduce(lambda value, i: value +(romans[s[i]] if romans[s[i]]>romans[s[i+1]] else -1*romans[s[i]]),reversed(range(len(s)-1)),romans[s[-1]])
+
+def romanToInt(s):
+    result=0
+    for i in range(len(s)):
+        if romans[s[i]] > romans[s[i-1]]:
+            result +=romans[s[i]]-2*romans[s[i-1]]
         else:
-            current_value=x
-            result+=romans.get(x)
+            result+=romans[s[i]]
 
         # if (x == "I"):
         #     current_value = "I"
@@ -44,3 +47,5 @@ if "__main__":
     string="MCMXCIV"
     result=romanToInt(string)
     print(result)
+    print(different_solving(string))
+
